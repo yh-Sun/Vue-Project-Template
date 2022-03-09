c7b0140c4a4c9cb861b098ccae91acc827e8b78b
孙元贺 <sunyuanhe1@jd.com>
2022-03-09 11:06:57 +0800

    添加Home页，用于存储头

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import installElementPlus from './plugins/element'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+
+app.use(store).use(router).mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -14,7 +14,13 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    // element plus的demo首页
+    path: '/el',
+    name: 'el',
+    component: () => import('../views/element-plus/Home.vue')
+  },
 ]
 
 const router = createRouter({
